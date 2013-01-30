aa56969f8e440912ddd34f9d234ca38349cc7934
tomoakiitoh <tomoaki@eastwind.ne.jp>
2013-01-30 15:40:18 +0900

    First Commit
    test

---
+++ estimate0001.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html> 
+<html> 
+	<head> 
+	<title>My Page</title> 
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.1/jquery.mobile-1.1.1.min.css" />
+	<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.1.1/jquery.mobile-1.1.1.min.js"></script>
+</head> 
+<body> 
+
+<div data-role="page">
+
+	<div data-role="header">
+		<h1>dragon 見積システム</h1>
+	</div><!-- /header -->
+
+	<div data-role="content">	
+		<p><strong>株式会社　ヨークベニマル</strong>(108301)</p>		
+		<div data-role="collapsible">
+		<h1>見積条件</h1>
+		<p>見積名称　ロッカー</p>
+		<p>売価ランク　0</p>
+		<p>標準掛率　48%</p>
+		<p>標準価格表示　する</p>
+		<p>納入期日　別途打ち合わせ</p>
+		<p>受渡場所　指定場所</p>
+		<p>お支払条件　従前通り</p>
+		<p>有効期限　2カ月</p>
+		</div>
+	</div><!-- /content -->
+
+	<ul data-role="listview">
+		<li data-role="list-divider">15人用ロッカー</li>
+		<li>
+			<h2>HL-15358N シューズロッカー　15人用(119401600)</h2>
+			<p>W800D350H930　Page340</p>
+			<p class="ui-li-count">数量1　単価39,460　計39,460</p>
+		</li>
+		<li>
+			<h2>送料(1999999300)</h2>
+			<p class="ui-li-count">数量1　単価3,000　計3,000</p>
+		</li>
+		<li>
+			<p class="ui-li-count">小計42,460</p>
+		</li>
+		<li data-role="list-divider">12人用ロッカー</li>
+		<li>
+			<h2>HL-12628 ロッカー　15人用(2列6段)(119401600)</h2>
+			<p>W1778D450H880　Page340</p>
+			<p class="ui-li-count">数量1　単価39,460　計39,460</p>
+		</li>
+		<li>
+			<h2>送料(1999999300)</h2>
+			<p class="ui-li-count">数量1　単価3,000　計3,000</p>
+		</li>
+		<li>
+			<p class="ui-li-count">小計45,960</p>
+		</li>
+		<li data-role="list-divider">
+			<h3>見積金額88,420</h3>
+		</li>
+		<li>
+			<p>原価合計67,160　利益金額21,260　利益率24.04%</p>
+		</li>
+	</ul>
+
+
+	<div data-role="content">	
+		<p><strong>東京第一営業所</strong></p>		
+		<p>池上　薫</p>
+		<p>作成日　2012/05/29</p>
+		<p>見積番号　70339</p>
+	</div><!-- /content -->
+
+
+</div><!-- /page -->
+
+</body>
+</html>
